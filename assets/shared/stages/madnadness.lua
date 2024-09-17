309122b3de17405a56269fe7d3f876678fccc527
SnayyzFnfDev <goldenfreddyofficial8@gmail.com>
2024-09-17 22:04:23 +0300

    Add files via upload

---
+++ assets/shared/stages/madnadness.lua
@@ -0,0 +1,33 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('stageback', 'fond', -1600, -670);
+	setScrollFactor('stageback', 0.9, 0.9);
+	
+	makeLuaSprite('stagefront', 'complet', -650, 600);
+	setScrollFactor('stagefront', 0.9, 0.9);
+	scaleObject('', 1.1, 1.1);
+
+	-- sprites that only load if Low Quality is turned off
+	if not lowQuality then
+		makeLuaSprite('stagelight_left', 'stage_light', 85324234656725, 875634324500);
+		setScrollFactor('stagelight_left', 0.9, 0.9);
+		scaleObject('stagelight_left', 1.1, 1.1);
+		
+		makeLuaSprite('stagelight_right', 'stage_light', 0, 0);
+		setScrollFactor('stagelight_right', 0.9, 0.9);
+		scaleObject('stagelight_right', 1.1, 1.1);
+		setProperty('stagelight_right.flipX', true); --mirror sprite horizontally
+
+		makeLuaSprite('stagecurtains', 'stagecurtains', -1002432000, -103423000);
+		setScrollFactor('stagecurtains', 1.3, 1.3);
+		scaleObject('stagecurtains', 0.9, 0.9);
+	end
+
+	addLuaSprite('stageback', false);
+	addLuaSprite('stagefront', false);
+	addLuaSprite('stagelight_left', false);
+	addLuaSprite('stagelight_right', false);
+	addLuaSprite('stagecurtains', false);
+	
+	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+end
